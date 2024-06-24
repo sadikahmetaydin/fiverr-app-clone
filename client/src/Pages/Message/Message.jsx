81c7c450630f90599f442b1bf6d8b5c7d437035f
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-06-24 20:45:46 +0300

    Fiverr App Clone Project Final

--- client/src/Pages/Message/Message.jsx
+++ client/src/Pages/Message/Message.jsx
@@ -1,11 +1,203 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // Style Connection
 import './Message.css';
 
 const Message = () => {
   return (
     <div className="message">
-      <h1>helloAdpage!</h1>
+      <div className="container">
+        <span className="breadCrumbs">
+          <Link to="/messages">MESSAGES</Link> & ANNA BELL &
+        </span>
+
+        <div className="messages">
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="write">
+          <textarea
+            placeholder="write a message..."
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/Pages/Messages/Messages.jsx
+++ client/src/Pages/Messages/Messages.jsx
@@ -1,11 +1,90 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // Style Connection
 import './Messages.css';
 
 const Messages = () => {
+  const message = ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nisi ipsam optio distinctio? Tempora, architecto dicta? Accusantium qui modi nemo quisquam ad, pariatur vitae?`;
+
   return (
     <div className="messages">
-      <h1>helloAdpage!</h1>
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+
+          <tr className="active">
+            <td>Anna Bell</td>
+
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 Day Ago</td>
+
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+
+          <tr className="active">
+            <td>John Doe</td>
+
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 hours ago</td>
+
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+
+          <tr>
+            <td>Elinor Good</td>
+
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+
+          <tr>
+            <td>Garner David </td>
+
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 days ago</td>
+          </tr>
+
+          <tr>
+            <td>Troy Oliver</td>
+
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 week ago</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };

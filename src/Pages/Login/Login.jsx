aa62391da80ee75a4df1e117a23dd1ab4154f5df
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-06-27 19:42:03 +0300

    Fiverr App Clone Project Final

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,29 +1,8 @@
-import React, { useState } from 'react';
-// Axios
-import axios from 'axios';
+import React from 'react';
 // Style Connection
 import './Login.css';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await axios.post('http://localhost:8800/api/auth/login', {
-        username,
-        password,
-      });
-      console.log(res.data); //
-    } catch (err) {
-      setError(err);
-      console.log(err); //
-    }
-  };
-
   return (
     <div className="login">
       <form onSubmit={handleSubmit}>
